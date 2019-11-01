0fa931625560991083e6e8a7484ad0d3afe38652
Eldar Aliiev <bc.zim.green@gmail.com>
2019-11-02 00:36:36 +0200

    Initial commit

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestAdd1 (t *testing.T) {
+    if add(2, 2) != 4 {
+        t.Error("2 + 2 != 4")
+    }
+}
+
+func TestAdd2 (t *testing.T) {
+    if add(3, 3) != 6 {
+        t.Error("3 + 3 != 6")
+    }
+}
+
+func TestAdd3 (t *testing.T) {
+    if add(4, 4) == 9 {
+        t.Error("4 + 4 != 9")
+    }
+}
